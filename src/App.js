8b9d9f0ab2d58778aa136e7017fd28cdc49b68f9
Nicole <nicoleho1314@gmail.com>
2024-04-22 19:32:53 +0800

    normal version 2

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [search, setSearch] = useState(null);
-  const [resultAudio, setResultAudio] = useState([]);
-  const [showAudio, setShowAudio] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
+  const [search, setSearch] = useState(""); //for the input search box
+  const [resultAudio, setResultAudio] = useState([]); //the retrieved audio files
+  const [showAudio, setShowAudio] = useState(false); //whether to show the audio tag or not
   const [play, setPlay] = useState({
     name: null,
     audio: null,
-  });
-  const [isPlaying, setIsPlaying] = useState(false);
+  }); //audio play state variable
+  const [isLoading, setIsLoading] = useState(false); //audio is loading or not
+  const [isPlaying, setIsPlaying] = useState(false); //audio is playing or not
+  //Pagination variables
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  // Slice the data array based on the current page
-  const currentPageData = resultAudio.slice(startIndex, endIndex);
+  const currentPageData = resultAudio.slice(startIndex, endIndex); // Slice the data array based on the current page
 
+  //clean up after clearing seach box
   useEffect(() => {
     if (!search) {
       setResultAudio([]);
       setShowAudio(false);
+      setCurrentPage(1);
+      setIsPlaying(false);
+      setIsLoading(false);
       setPlay({
         name: null,
         audio: null,
@@ -29,9 +34,24 @@ function App() {
     }
   }, [search]);
 
+  //Event Listener to handle when audio file is not loaded completely
+  useEffect(() => {
+    const aud = document.getElementById(play.audio);
+    aud?.addEventListener("waiting", onAudioWait);
+    aud?.addEventListener("canplaythrough", onCanPlayThrough);
+  }, [play.audio]);
+
+  const onAudioWait = () => {
+    setIsLoading(true);
+  };
+
+  const onCanPlayThrough = () => {
+    setIsLoading(false);
+  };
+
+  //Function to call to retrieve relevant data after Enter button is pressed
   const onHandleEnterPressed = async (key) => {
-    if (key.key === "Enter") {
-      // console.log("ENTERRRRRR");
+    if (key.key === "Enter" && search != "") {
       try {
         const response = await fetch(
           `https://freesound.org/apiv2/search/text/?fields=id,name,previews&query=${encodeURIComponent(
@@ -45,17 +65,18 @@ function App() {
         );
         if (response.ok) {
           const data = await response.json();
-          console.log(data.results);
+          // console.log(data.results);
           setResultAudio(data.results);
         } else {
           throw new Error("Error fetching search results");
         }
       } catch (e) {
-        console.log(e);
+        throw new Error("Error fetching search results");
       }
     }
   };
 
+  //when clicked on an audio
   const onClickPlay = (r) => {
     setShowAudio(true);
     setPlay((prev) => ({
@@ -65,9 +86,10 @@ function App() {
     }));
   };
 
+  //Paginate functions
   const goToPreviousPage = () => {
     if (!isPlaying && currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+      setCurrentPage((prev) => prev - 1);
     }
   };
 
@@ -75,11 +97,12 @@ function App() {
     if (!isPlaying) {
       const totalPages = Math.ceil(resultAudio.length / itemsPerPage);
       if (currentPage < totalPages) {
-        setCurrentPage(currentPage + 1);
+        setCurrentPage((prev) => prev + 1);
       }
     }
   };
 
+  //Functions for monitoring the disabling pagination and input
   const handleAudioPlay = () => {
     setIsPlaying(true);
   };
@@ -128,12 +151,16 @@ function App() {
       {showAudio && (
         <>
           <p>Now Playing: {play.name}</p>
-          <audio
-            controls
-            src={play.audio}
-            onPlay={handleAudioPlay}
-            onPause={handleAudioPause}
-          ></audio>
+          {isLoading ? (
+            <div>Audio Loading...</div>
+          ) : (
+            <audio
+              controls
+              src={play.audio}
+              onPlay={handleAudioPlay}
+              onPause={handleAudioPause}
+            ></audio>
+          )}
         </>
       )}
     </div>
